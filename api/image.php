55cf0697d6bb59dbbbdd09409e2b107a482fa10b
MonoKelvin <15007083506@qq.com>
2020-02-05 16:38:39 +0800

    添加获取用户头像api

---
+++ api/image.php
@@ -0,0 +1,82 @@
+<?php
+
+$user = @$_GET['user'];
+$type = @$_GET['type'];
+$id = @$_GET['id'];
+
+// 如果是用户
+if ($user == 'user') {
+    // 如果id存在
+    if ($id) {
+        header('Content-Type:image/png');
+        $imgsrc = 'http://inkchat.com/images/avatar/' . $id . '.png';
+
+        if ($type == 'avatar') {
+            if (!@readfile($imgsrc)) {
+                header('HTTP/1.1 404 Not Found');
+                header('Status: 404 Not Found');
+                exit;
+            }
+        } else if ($type == 'thumb') {
+            // 如果要获取的是缩略图，则处理图片为 45*45
+            avatarResize($imgsrc);
+        }
+    }
+}
+
+/**
+ * 重设头像尺寸
+ *
+ * @param string $file_name 文件名
+ *
+ * 包含全路径（可以是相对路径和 web地址）和文件名称
+ *
+ * @param int $newsize [optional]
+ *
+ * 新尺寸，尽量`比原尺寸小`，因为该函数会`降低`缩小后图片的质量。另外该方法主要针对`等宽高`的头像图片
+ * 所以，参数中不提供分别对宽高设置的内容，即默认`等比例缩放`。
+ *
+ * @return boolean 如果重设成功则返回 true，否则返回 false
+ *
+ */
+function avatarResize($file_name, $newsize = 45)
+{
+    // 返回一个数组，第一个元素 (索引值 0) 是图片的高度，单位是像素 (pixel)。
+    // 第二个元素 (索引值 1) 是图片的宽度。
+    // 第三个元素 (索引值 2) 是图片的文件格式，其值 1 为 GIF 格式、 2 为 JPEG/JPG 格式、3 为 PNG 格式。
+    // 第四个元素 (索引值 3) 为图片的高与宽字符串，height=xxx width=yyy。
+    $size = @getimagesize($file_name) or die('Read image file is failed');
+
+    // 声明一个真彩图片资源，此时只是一个有宽高的黑白图片。
+    $newimg = @imagecreatetruecolor($newsize, $newsize) or die('Cannot initialize a GD image stream');
+
+    // 根据图片的格式使用不同的方法创建为新的图片
+    if ($size[2] == 1) $src = imagecreatefromgif($file_name);
+    if ($size[2] == 2) $src = imagecreatefromjpeg($file_name);
+    if ($size[2] == 3) {
+        $src = imagecreatefrompng($file_name);
+
+        // 不丢失图像的透明色
+        imagesavealpha($src, true);
+        // 不合并颜色，直接用原图像颜色替换，包括透明色。
+        imagealphablending($newimg, false);
+        // 是不要丢了$newimg的透明色
+        imagesavealpha($newimg, true);
+    }
+
+    // 参数（目标资源，源，目标资源的开始坐标x,y, 源资源的开始坐标x,y,目标资源的宽高w,h,源资源的宽高w,h）返回值为 bool。
+    imagecopyresampled($newimg, $src, 0, 0, 0, 0, $newsize, $newsize, $size[1], $size[0]);
+
+    // header('Content-Type: image/png');
+
+    // 将图片塞进去，根据图片不同的格式选用不同的方法，同时降低适当质量。
+    if ($size[2] == 1) $result = @imagegif($newimg, null, 6);
+    if ($size[2] == 2) $result = @imagejpeg($newimg, null, 6);
+    if ($size[2] == 3) $result = @imagepng($newimg, null, 6);
+
+    // 销毁资源
+    @imagedestroy($newimg);
+
+    // 返回生成好的图片
+    return $result;
+}
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>InkChat API</title>
+</head>
+
+<body>
+    InkChat Api
+    <ul>
+        <li>
+            获取图片：
+            <form action="./api/image.php" method="get">
+                <div>
+                    <input name="user" type="text" value="user">
+                    <input name="type" type="text" value="avatar">
+                    <input name="id" type="number" value="1">
+                    <button type="submit">测试</button>
+                </div>
+            </form>
+        </li>
+    </ul>
+</body>
+
+</html>
